41783336076664432772978689207379e3abad99
Nate Whittaker <nwhittaker@esri.com>
2025-10-15 12:08:19 -0400

    Install and setup `@stencil/playwright`

--- stencil.config.ts
+++ stencil.config.ts
@@ -18,6 +18,7 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null, // disable service workers
+      copy: [{ src: '**/*.html' }, { src: '**/*.css' }],
     },
   ],
   testing: {
